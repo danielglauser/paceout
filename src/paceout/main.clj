27ce96c715916cc4ea5f60ed58f32483297b1cf2
Daniel Glauser <danglauser@gmail.com>
2015-01-13 01:11:17 +0000

    [#phase-1] +5 parsers of resiliance

--- src/paceout/main.clj
+++ src/paceout/main.clj
@@ -1,8 +1,10 @@
 (ns paceout.main
   "The entrypiont for the paceout account number parser. Handles
   command line arguments and the top level control flow."
-  (:require [clojure.string :as string]
-            [clojure.tools.cli :as cli])
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.cli :as cli]
+            [paceout.parser :as parser])
   (:gen-class))
 
 (def cli-options [["-h" "--help"]
@@ -42,6 +44,16 @@
   (str "The following errors occurred while parsing:\n\n"
        (string/join "\n" errors)))
 
+(defn get-reader [filename]
+  (-> filename io/file io/reader))
+
+(defn get-writer
+  "Creates a writer to the optional filename. Defaults to stdout."
+  [& [filename]]
+  (if filename
+    (-> filename io/file io/writer)
+    (io/writer *out*)))
+
 (defn -main [& args]
   ;; options are parsed :in-order so we handle help first
   (let [{:keys [options arguments errors summary]}
@@ -50,4 +62,10 @@
      (:help options) (exit 0 (usage summary))
      errors (exit 1 (error-msg errors))
      (= 0 (count arguments)) (exit 1 (usage summary)))
-    (println "Did it.")))
+
+    (with-open [in (get-reader (first arguments))
+                out (get-writer)]
+      (try
+        (parser/parse-account-numbers in out)
+        (catch Exception ex
+          (println "Error when parsing: "(.getMessage ex)))))))
--- test/paceout/test/parser.clj
+++ test/paceout/test/parser.clj
@@ -1,4 +1,77 @@
 (ns paceout.test.parser
   "Clojure.test tests for the parser namespace."
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [paceout.parser :refer :all]))
+
+(deftest test-parse-column
+  (is (= [["a" "d"] ["bc" "ef"]]
+         (parse-column 1 ["abc" "def"])))
+  (is (= [["ab" "de"] ["c" "f"]]
+         (parse-column 2 ["abc" "def"])))
+  (is (= [["abc" "def"] ["" ""]]
+         (parse-column 3 ["abc" "def"]))))
+
+(deftest test-safe-subs
+  (is (= "d" (safe-subs "abcd" 3)))
+  (is (= "" (safe-subs "abc" 3)))
+  (is (= nil (safe-subs nil 3))))
+
+(deftest test-parse-account-number*
+  (is (= "000000000"
+         (parse-account-number* [" _  _  _  _  _  _  _  _  _ "
+                                 "| || || || || || || || || |"
+                                 "|_||_||_||_||_||_||_||_||_|"])))
+  (is (= "912345678"
+         (parse-account-number* [" _     _  _     _  _  _  _ "
+                                 "|_|  | _| _||_||_ |_   ||_|"
+                                 "  |  ||_  _|  | _||_|  ||_|"])))
+  (is (= "" (parse-account-number* nil))))
+
+(deftest test-parse-acount-numbers
+  (with-open [rdr (-> (io/resource "sample_account_numbers.txt") io/reader)
+              wtr (java.io.StringWriter.)]
+    (parse-account-numbers rdr wtr)
+    (let [account-numbers (.toString wtr)]
+      (is (= (str "000000000\n111111111\n333333333\n444444444\n555555555\n"
+                  "666666666\n777777777\n888888888\n999999999\n123456789\n")
+             account-numbers)))))
+
+(deftest test-parse-digit
+  (is (= "0" (parse-digit [" _ "
+                           "| |"
+                           "|_|"])))
+  (is (= "1" (parse-digit ["   "
+                           "  |"
+                           "  |"])))
+  (is (= "2" (parse-digit [" _ "
+                           " _|"
+                           "|_ "])))
+  (is (= "3" (parse-digit [" _ "
+                           " _|"
+                           " _|"])))
+  (is (= "4" (parse-digit ["   "
+                           "|_|"
+                           "  |"])))
+  (is (= "5" (parse-digit [" _ "
+                           "|_ "
+                           " _|"])))
+  (is (= "6" (parse-digit [" _ "
+                           "|_ "
+                           "|_|"])))
+  (is (= "7" (parse-digit [" _ "
+                           "  |"
+                           "  |"])))
+  (is (= "8" (parse-digit [" _ "
+                           "|_|"
+                           "|_|"])))
+  (is (= "9" (parse-digit [" _ "
+                           "|_|"
+                           "  |"])))
+  (is (= "?" (parse-digit ["   "
+                           " _|"
+                           "  |"])))
+  (is (= "?" (parse-digit [" "
+                           "_|"
+                           "  |"])))
+  (is (= nil (parse-digit ["" "" ""]))))
